dd4ec2913d674065098d3a64273a38ea52ffb75f
ahmedalnaqaa <ahmedalnaqaa@gmail.com>
2020-05-11 04:12:27 +0200

    Set default redirection url after login

--- src/Handler/LoginSuccessHandler.php
+++ src/Handler/LoginSuccessHandler.php
@@ -32,6 +32,7 @@ class LoginSuccessHandler implements AuthenticationSuccessHandlerInterface
      */
     public function onAuthenticationSuccess(Request $request, TokenInterface $token)
     {
+        $url = 'index';
         if (!in_array('ROLE_DOCTOR', $token->getUser()->getRoles()) && sizeof($token->getUser()->getRoles()) == 1) {
             $url = 'user-isolations';
         } elseif (in_array('ROLE_DOCTOR', $token->getUser()->getRoles())) {
