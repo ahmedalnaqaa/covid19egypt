dbb01b292e9a0426ece2b774b16c6bb2d1903442
ahmedalnaqaa <ahmedalnaqaa@gmail.com>
2020-05-25 08:52:12 +0200

    Set default score value into database

--- src/Migrations/Version20200525062524.php
+++ src/Migrations/Version20200525062524.php
@@ -22,7 +22,7 @@ final class Version20200525062524 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE blacklists ADD score SMALLINT DEFAULT 0 NOT NULL');
+        $this->addSql('ALTER TABLE blacklists ADD score SMALLINT DEFAULT 1 NOT NULL');
     }
 
     public function down(Schema $schema) : void
