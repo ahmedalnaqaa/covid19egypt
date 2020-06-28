5cdab0263fc46749b036927f0f513828f0ab1399
ahmedalnaqaa <ahmedalnaqaa@gmail.com>
2020-06-28 12:28:26 +0200

    Improve estimates

--- src/EventListener/ExceptionListener.php
+++ src/EventListener/ExceptionListener.php
@@ -42,8 +42,6 @@ class ExceptionListener
         if(strpos($ip,',') !== false) {
             $ip = substr($ip,0,strpos($ip,','));
         }
-        echo getenv('APP_ENV');
-        exit();
         if ($ip) {
             /** @var BlackList $blacklisted */
             $blacklisted = $this->em->getRepository('App:BlackList')->findOneByIp($ip);
